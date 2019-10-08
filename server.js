749637c66cc3341695972a7483080dd303e9758f
Jeremy Karlsson <karlsson@jeremy.se>
2019-10-08 22:29:42 +0200

    Remove loop for uuid

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const sio = require('socket.io');
 const uuid = require('node-uuid');
 const open = require('open');
 const express = require('express');
-const path = require('path');
 const app = express();
 const http = require('http').Server(app);
 
-app.use(express.static('src'))
+app.use(express.static('src'));
 
 // Create and configure socket.io
 const io = sio.listen(http, { log: true });
@@ -22,20 +21,14 @@ http.listen(port, () => {
 const sockets = {};
 
 io.sockets.on('connection', socket => {
-  let id;
+  let id = uuid.v4();
 
-	// Get UUID
-
-  do {
-    id = uuid.v4();
-  } while (sockets[id]);
-
-	// we have a unique identifier that can be sent to the client
+  // we have a unique identifier that can be sent to the client
 
   sockets[id] = socket;
   socket.emit('your-id', id);
 
-	// remove references to the disconnected socket
+  // remove references to the disconnected socket
   socket.on('disconnect', () => {
     sockets[socket] = undefined;
     delete sockets[socket];
@@ -47,7 +40,7 @@ io.sockets.on('connection', socket => {
     socket.emit('your-id', id);
   });
 
-	// when a message is received forward it to the addressee
+  // when a message is received forward it to the addressee
   socket.on('message', message => {
     if (sockets[message.to]) {
       sockets[message.to].emit('message', message);
@@ -56,7 +49,7 @@ io.sockets.on('connection', socket => {
     }
   });
 
-	// when a listener logs on let the media streaming know about it
+  // when a listener logs on let the media streaming know about it
   socket.on('logon', message => {
     if (sockets[message.to]) {
       sockets[message.to].emit('logon', message);
